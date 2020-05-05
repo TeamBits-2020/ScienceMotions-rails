2e1dd2b03d84bab3d6c71bc44391601389320e59
Aline Ribeiro <alinear.mail@gmail.com>
2020-05-05 11:19:55 -0700

    Render buttons

--- app/javascript/bundles/Container/components/lessons/AlkaliMetals.js
+++ app/javascript/bundles/Container/components/lessons/AlkaliMetals.js
@@ -5,24 +5,24 @@ import Text from "./text/Text";
 const AlkaliMetals = props => {
   return (
     <div>
-      {/*<section id="top">*/}
-      {/*  <a style="float:left;" className="fond button-flip" href="#text">*/}
-      {/*    <button style="color: #fff; margin: 50px;" type="button" className="btn" id="to-ammend" href="#text">More*/}
-      {/*      info*/}
-      {/*    </button>*/}
-      {/*  </a>*/}
-      {/*  <div className="main">*/}
-      {/*    <div id="container"></div>*/}
-      {/*    <div id="menu">*/}
-      {/*      <a className="button-flip">*/}
-      {/*        <button id="helix" style="color: #fff;" className="btn">VIEW ALKALI METALS</button>*/}
-      {/*      </a>*/}
-      {/*      <a className="button-flip">*/}
-      {/*        <button id="table">TABLE VIEW</button>*/}
-      {/*      </a>*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</section>*/}
+      <section id="top">
+        <a style={{float:"left"}} className="fond button-flip" href="#text">
+          <button style={{color: "#fff", margin: "50px"}} type="button" className="btn" id="to-ammend" href="#text">More
+            info
+          </button>
+        </a>
+        <div className="main">
+          <div id="container"></div>
+          <div id="menu">
+            <a className="button-flip">
+              <button id="helix" className="btn" style={{color: "#fff"}}>VIEW ALKALI METALS</button>
+            </a>
+            <a className="button-flip">
+              <button id="table">TABLE VIEW</button>
+            </a>
+          </div>
+        </div>
+      </section>
       <Text props={props.description} />
     </div>
   );
